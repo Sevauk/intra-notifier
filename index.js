9a504daa40fbd889ee7451f85b45b521c6c518e0
Adrien morel <adrien.morel@me.com>
2016-09-23 21:46:28 +0200

    final script

--- index.js
+++ index.js
@@ -2,14 +2,6 @@ var request = require('request')
 var SlackBot = require('slackbots');
 const fs = require('fs')
 
-
-
-var jsdom = require("jsdom").jsdom;
-var doc = jsdom();
-var window = doc.defaultView;
-
-const $ = require('jquery')(window)
-
 const http = require('https')
 
 var cookieJar = request.jar();
@@ -32,38 +24,48 @@ const notif = title => {
 
   // create a bot
   var bot = new SlackBot({
-      token: 'xoxb-36845936224-Hmcsn5wmrGZV7z8UxkBvkM26', // Add a bot https://my.slack.com/services/new/bot and put the token
+      token: 'xoxb-36845936224-gAFAStUZYaKbwZzLEqTdCfal', // Add a bot https://my.slack.com/services/new/bot and put the token
+      name: 'jarvis'
   });
 
-/*
   bot.on('start', function() {
       // more information about additional params https://api.slack.com/methods/chat.postMessage
       var params = {
       };
 
       // define channel, where bot exist. You can adjust it there https://my.slack.com/services
-      bot.postMessageToChannel('general', 'meow!', params);
+      bot.postMessageToChannel('urgence', title, params)
+        .then(() => process.exit())
   })
-  */
 }
 
 const detectNotif = json => {
   fs.readFile('last.txt', (err, res) => {
-    const id = res.toString()
-    const newId = json[0].id
 
-    notif(json[0].title)
 
-    if (id === newId) {
-      console.log('no update')
-      return
+    const moduleslen = json['board']['modules'].length
+    const notifs = json['history']
+
+    if (!err) {
+
+      const last = JSON.parse(res.toString())
+
+      const id = last.notifid
+      const newId = notifs[0].id
+
+      if (id !== newId) {
+        notif('Une nouvelle notification est apparue sur l\'intra d\'Adrien. ' + notifs[0].title)
+      }
+      if (last.moduleslen != moduleslen) {
+        notif('Des modules ont ouvert (total: ' + moduleslen + ' modules)')
+      }
     }
 
-    fs.writeFile('last.txt', newId)
+    fs.writeFile('last.txt', JSON.stringify({'notifid': notifs[0].id, moduleslen}))
   })
 }
 
 get('https://intra.epitech.eu/auth-2991948b7e99b1d6482ce93f803b44d17ef73822')
   .catch(err => console.log("Got error: " + err.message))
-  .then(() => get('https://intra.epitech.eu/user/notification/message?format=json'))
+  .then(() => get('https://intra.epitech.eu/?format=json'))
   .then(json => detectNotif(JSON.parse(json)))
